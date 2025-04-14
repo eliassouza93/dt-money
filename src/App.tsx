1566f5503c1c91bc99e3cd75246880544371cedc
Elias de Souza <elias99-souza@hotmail.com>
2025-04-14 16:10:04 -0300

    'dt-money'

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
+
+
+function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+     
+    </ThemeProvider>
+  )
+}
+
+export default App
